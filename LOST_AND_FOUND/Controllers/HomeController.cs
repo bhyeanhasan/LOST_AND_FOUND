f08de67029cb58c3e09238ab74abc92448e83355
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2022-06-14 18:42:52 +0600

    Add project files.

---
+++ LOST_AND_FOUND/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using LOST_AND_FOUND.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LOST_AND_FOUND.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LOST_AND_FOUND/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LOST_AND_FOUND.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
